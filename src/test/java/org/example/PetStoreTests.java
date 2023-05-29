9ab9ccc76518148b4d72253935055129ea8e7860
Ahmad J. Abu Yahia <45077440+software-ace@users.noreply.github.com>
2023-05-29 13:46:54 +0300

    added Data Provider to Filling the Contact Form Test.

--- src/test/java/org/example/PetStoreTests.java
+++ src/test/java/org/example/PetStoreTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.*;
 
@@ -14,6 +15,29 @@ import java.time.Duration;
 import java.util.Random;
 
 public class PetStoreTests {
+    @DataProvider(name = "contactFormData")
+    public Object[][] getContactFormData() {
+        return new Object[][]{
+                {
+                        "Carl",
+                        "a@vogf.com",
+                        "55292500611",
+                        "I want my order wrapped as a gift"},
+                {
+                        "Gentry Torres",
+                        "gentrytorres@conferia.com",
+                        "8665533942",
+                        "Veniam cillum elit labore enim. Proident ullamco ipsum ut id aliquip id ad nostrud aliquip. Pariatur dolore esse occaecat adipisicing reprehenderit excepteur pariatur magna est commodo incididunt velit. Officia id mollit velit adipisicing reprehenderit aliquip.\r\n"
+                },
+                {
+                        "Wilcox Kirby",
+                        "wilcoxkirby@conferia.com",
+                        "8974362501",
+                        "Irure amet elit incididunt dolor. Eu nulla proident ex cupidatat. Do sunt eu ut et enim ex consequat non aute excepteur nisi.\r\n"
+                }
+        };
+    }
+
     private WebDriver driver;
     WebDriverWait webDriverWait;
     Random random = new Random();
@@ -53,7 +77,8 @@ public class PetStoreTests {
     public void verify_selecting_random_animal_category() {
         homePage.selectRandomPetType();
         int petItemsNumber = homePage.getPetItemsNumber();
-        Assert.assertTrue(petItemsNumber == 10 || petItemsNumber == 12, "Unexpected number of items: " + petItemsNumber);
+        Assert.assertTrue(petItemsNumber == 10 || petItemsNumber == 12,
+                "Unexpected number of items: " + petItemsNumber);
     }
 
     @Test(priority = 3)
@@ -73,9 +98,9 @@ public class PetStoreTests {
         Assert.assertTrue(done);
     }
 
-    @Test(priority = 5)
-    public void verify_filling_contact_form_and_submit() {
-        contactPage.fillForm("Carl", "a@vogf.com", "55292500611", "I want my order wrapped as a gift");
+    @Test(priority = 5, dataProvider = "contactFormData")
+    public void verify_filling_contact_form_and_submit(String name, String email, String phone, String message) {
+        contactPage.fillForm(name, email, phone, message);
         contactPage.submitForm();
         Assert.assertEquals(contactPage.EXPECTED_MSG, contactPage.getFormMessage());
     }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -16,25 +16,19 @@ public class HomePage {
     String PAGE_URL = "https://ecom-pet-store.myshopify.com/";
     @FindBy(className = "collection-grid__item-link")
     private List<WebElement> petTypes;
-
     @FindBy(className = "product--image")
     private List<WebElement> petItems;
-
     public HomePage(WebDriver d, Random r) {
         driver = d;
         random = r;
         PageFactory.initElements(d, this);
     }
-
     public void selectRandomPetType() {
         driver.get(PAGE_URL);
         int randomPetType = random.nextInt(4);
         petTypes.get(randomPetType).click();
-
     }
-
     public int getPetItemsNumber() {
         return petItems.size();
     }
-
 }
